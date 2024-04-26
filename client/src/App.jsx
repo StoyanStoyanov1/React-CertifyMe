0be722c82ae95cd7fabadd7e887a33df09b9523c
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-04-26 10:37:30 +0200

    CertifyMe

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import {Routes} from "react-router-dom";
+import Header from "./components/header/Header.jsx";
+
+function App() {
+
+  return (
+      <div id='box'>
+          <Header/>
+          <Routes>
+
+
+          </Routes>
+
+      </div>
+
+  )
+}
+
+export default App
---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+export default function Header() {
+	return(
+		<header>
+			<nav>
+				<a href="/">CertifyMe</a>
+				<ul>
+					<li><a href="#">Мy certificates</a></li>
+					<li><a href="#">Login</a></li>
+					<li><a href="#">Register</a></li>
+					<li><a href="#">Add certificate</a></li>
+					<li><a href="#">Logout</a></li>
+				</ul>
+			</nav>
+		</header>
+	)
+}
+
