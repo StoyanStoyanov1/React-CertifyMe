4df9f041e015ba82095d598c66517c430ea26075
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-04-26 11:08:41 +0200

    home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,21 @@
-import { useState } from 'react'
-import {Routes} from "react-router-dom";
+import {useState} from 'react'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Header from "./components/header/Header.jsx";
+import Path from "./paths.js";
+import Home from "./components/home/Home.jsx";
 
 function App() {
 
-  return (
-      <div id='box'>
-          <Header/>
-          <Routes>
+	return (
+		<div id='box'>
+			<Header/>
+			<Routes>
+				<Route path={Path.Home} element={<Home/>}/>
 
+			</Routes>
 
-          </Routes>
-
-      </div>
-
-  )
+		</div>
+	)
 }
 
 export default App
---
+++ client/src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+export default function Home() {
+	return (
+		<section id="welcomePage">
+			<div id="welcome-message">
+				<h1>Welcome to CertifyMe</h1>
+			</div>
+
+			<div class="music-img">
+				<img src="./images/certifikate-icons.jpg"/>
+			</div>
+		</section>
+	)
+}
