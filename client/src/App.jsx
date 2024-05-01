f4efb702454e26fece4cf33e37c819530fc5fff3
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-05-01 08:45:28 +0200

    auth context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,24 +10,29 @@ import MyCertificates from "./components/myCerificates/MyCertificates.jsx";
 import CertificateDetails from "./components/certificateDetails/CertificateDetails.jsx";
 import EditCertificate from "./components/editCertificate/EditCertificate.jsx";
 import Footer from "./components/footer/Footer.jsx";
+import AuthContext from "./context/authContext.js";
 
 function App() {
 
-	return (
-		<div id='box'>
-			<Header/>
-			<Routes>
-				<Route path={Path.Home} element={<Home/>}/>
-				<Route path={Path.Login} element={<Login/>}/>
-				<Route path={Path.Register} element={<Register />}/>
-				<Route path={Path.AddCertificate} element={<AddCertificate />}/>
-				<Route path={Path.MyCertificates} element={<MyCertificates />}/>
-				<Route path={`${Path.MyCertificates}/:certificateId`} element={<CertificateDetails />} />
-				<Route path={Path.EditCertificate} element={<EditCertificate />} />
-			</Routes>
-			<Footer/>
+	const values = {};
 
-		</div>
+	return (
+		<AuthContext value={values}>
+			<div id='box'>
+				<Header/>
+				<Routes>
+					<Route path={Path.Home} element={<Home/>}/>
+					<Route path={Path.Login} element={<Login/>}/>
+					<Route path={Path.Register} element={<Register />}/>
+					<Route path={Path.AddCertificate} element={<AddCertificate />}/>
+					<Route path={Path.MyCertificates} element={<MyCertificates />}/>
+					<Route path={`${Path.MyCertificates}/:certificateId`} element={<CertificateDetails />} />
+					<Route path={Path.EditCertificate} element={<EditCertificate />} />
+				</Routes>
+				<Footer/>
+
+			</div>
+		</AuthContext>
 	)
 }
 
