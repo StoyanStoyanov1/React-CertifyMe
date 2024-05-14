8e76ed7e4788037e096fa12ee0621aea14a3c34b
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-05-14 07:40:42 +0200

    update myCertificates

--- client/src/components/myCerificates/MyCertificates.jsx
+++ client/src/components/myCerificates/MyCertificates.jsx
@@ -1,26 +1,28 @@
-import {useEffect, useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import * as certificateServer from '../../services/certificateService.js'
 import CertificateItem from "./CertificateItem.jsx";
 import {Link} from "react-router-dom";
 import Path from "../../paths.js";
+import AuthContext from "../../context/authContext.jsx";
 
 export default function MyCertificates() {
 	const [certificates, setCertificates] = useState([]);
+	const {_id} = useContext(AuthContext);
 
 	useEffect(() => {
 		certificateServer.getAll()
 			.then(result => setCertificates(result))
 	}, []);
 
+	const myCertificates = certificates.filter(cer => cer._ownerId === _id);
+
 	return (
 		<section id="catalogPage">
 			<h1>My certificates</h1>
 
-			{certificates.map(cer => (
-				<CertificateItem key={cer._id} {...cer}/>
+			{myCertificates.length === 0 ? (
+				<p>No found certificates!</p>) : myCertificates.map(cer => (<CertificateItem key={cer._id} {...cer}/>
 			))}
-
-			{certificates.length === 0 && (<p>No found certificates!</p>)}
 
 		</section>
 	)
