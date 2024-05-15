d30c9e977a04b60ae54c71fee83e973a69cd7f81
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-05-15 16:19:04 +0200

    Error Boundary

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,35 +20,37 @@ import AllProfiles from "./components/allProfiles/AllProfiles.jsx";
 import DetailProfil from "./components/allProfiles/DetailProfil.jsx";
 import EditProfile from "./components/allProfiles/EditProfile.jsx";
 import MyProfil from "./components/allProfiles/MyProfil.jsx";
+import ErrorBoundary from "./components/ErrorBoundary.jsx";
 
 function App() {
 
 	return (
+		<ErrorBoundary>
+			<AuthProvider>
+				<div id='box'>
+					<Header/>
 
-		<AuthProvider>
-			<div id='box'>
-				<Header/>
+					<Routes>
+						<Route path={Path.Home} element={<Home/>}/>
+						<Route path={Path.Login} element={<Login/>}/>
+						<Route path={Path.Register} element={<Register/>}/>
+						<Route path={Path.AddCertificate} element={<AddCertificate/>}/>
+						<Route path={Path.MyCertificates} element={<MyCertificates/>}/>
+						<Route path={Path.AllCertificate} element={<AllCertificate/>}/>
+						<Route path={`${Path.MyCertificates}/:certificateId`} element={<CertificateDetails/>}/>
+						<Route path={`${Path.EditCertificate}/:certificateId`} element={<EditCertificate/>}/>
+						<Route path={Path.Logout} element={<Logout/>}/>
+						<Route path={`${Path.Remove}/:certificateId`} element={<RemoveCertificate/>}/>
+						<Route path={Path.AllProfiles} element={<AllProfiles/>}/>
+						<Route path={`${Path.Profil}/:profilId`} element={<DetailProfil/>}/>
+						<Route path={`${Path.EditProfile}/:profilId`} element={<EditProfile/>}/>
+						<Route path={Path.MyProfil} element={<MyProfil/>}/>
+					</Routes>
 
-				<Routes>
-					<Route path={Path.Home} element={<Home/>}/>
-					<Route path={Path.Login} element={<Login/>}/>
-					<Route path={Path.Register} element={<Register />}/>
-					<Route path={Path.AddCertificate} element={<AddCertificate />}/>
-					<Route path={Path.MyCertificates} element={<MyCertificates />}/>
-					<Route path={Path.AllCertificate} element={<AllCertificate/>}/>
-					<Route path={`${Path.MyCertificates}/:certificateId`} element={<CertificateDetails />} />
-					<Route path={`${Path.EditCertificate}/:certificateId`} element={<EditCertificate />} />
-					<Route path={Path.Logout} element={<Logout />} />
-					<Route path={`${Path.Remove}/:certificateId`} element={<RemoveCertificate />}/>
-					<Route path={Path.AllProfiles} element={<AllProfiles/>} />
-					<Route path={`${Path.Profil}/:profilId`} element={<DetailProfil/>}/>
-					<Route path={`${Path.EditProfile}/:profilId`} element={<EditProfile/>}/>
-					<Route path={Path.MyProfil} element={<MyProfil/>}/>
-				</Routes>
-
-				<Footer/>
-			</div>
-		</AuthProvider>
+					<Footer/>
+				</div>
+			</AuthProvider>
+		</ErrorBoundary>
 	)
 }
 
