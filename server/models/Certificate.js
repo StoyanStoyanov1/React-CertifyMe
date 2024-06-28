1f3e59b7b091f12e9fc18baf42782e428bc52507
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-06-28 08:48:00 +0200

    validate certificate model

--- server/models/Certificate.js
+++ server/models/Certificate.js
@@ -4,22 +4,52 @@ const certificateSchema = mongoose.Schema({
 	title: {
 		type: String,
 		require: true,
+		validator: {
+			validate: (value) => {
+				return value.length > 2 && value.length < 12;
+			},
+			message: 'Title must be between 2 and 12 characters!'
+		}
 	},
 	start: {
 		type: String,
 		require: true,
+		validate: {
+			validator: (value) => {
+				return  /^(\d{2})\.(\d{2})\.(\d{4})$/.test(value);
+			},
+			message: 'Date is not a valid Format, muss be dd.MM.yyyy!'
+		}
 	},
 	end: {
 		type: String,
 		require: true,
+		validate: {
+			validator: (value) => {
+				return  /^(\d{2})\.(\d{2})\.(\d{4})$/.test(value);
+			},
+			message: 'Date is not a valid Format, muss be dd.MM.yyyy!'
+		}
 	},
 	university: {
 		type: String,
 		require: true,
+		validate: {
+			validator: (value) => {
+				return value.length > 2 && value.length < 10;
+			},
+			message: 'University muss be between 2 and 10 characters!'
+		}
 	},
 	imgUrl: {
 		type: String,
 		require: true,
+		validate: {
+			validator: (value) => {
+				return /^https?:\/\//.test(value)
+			},
+			message: 'Url must start with http:// or https://'
+		}
 	},
 	description: {
 		type: String,
