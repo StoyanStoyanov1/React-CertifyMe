43a9033b930a9803b1500640be44b3b4be111a4f
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-07-08 20:22:49 +0200

    count likes

--- client/src/components/allProfiles/DetailProfil.jsx
+++ client/src/components/allProfiles/DetailProfil.jsx
@@ -11,6 +11,7 @@ export default function DetailProfil() {
 	const {_id} = useContext(authContext);
 	const [profil, setProfil] = useState({});
 	const {profilId} = useParams();
+	const [countLikes, setCountLikes] = useState(0);
 
 	useEffect(() => {
 		profilService.getByUserId(profilId)
@@ -31,6 +32,10 @@ export default function DetailProfil() {
 
 	};
 
+	useEffect(() => {
+		setCountLikes(profil.likes? profil.likes.length: 0)
+	})
+
 	return (
 		<section id="detailsPage">
 			<div className="wrapper">
@@ -43,13 +48,14 @@ export default function DetailProfil() {
 						<p>{profil.description}</p>
 						<Link className='my-certificates' to={`${Path.MyCertificates}/${profil.userId}`}>My
 							Certificates</Link>
+						<p>Likes: {countLikes}</p>
 					</div>
 
 					<div className="actionBtn">
 						{profil.userId !== _id && (!isLiked(_id, profil.userId, profil.likes)
 							? <button className='like-button' onClick={() => handleLike(_id, 'like')}>Like</button>
 							: <button className='like-button'
-									  onClick={() => handleLike(_id, 'dislike')}>Dislike</button>)}
+									  onClick={() => handleLike(_id, 'dislike')}>Unlike</button>)}
 						{profil.userId === _id &&
 							<Link to={`${Path.EditProfile}/${profilId}`} className="edit">Edit</Link>}
 					</div>
