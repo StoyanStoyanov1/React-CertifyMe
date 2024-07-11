0456dac29aa6bb82263794d0781cc9365c287190
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-07-11 20:28:17 +0200

    add a page by users

--- client/src/components/allProfiles/AllProfiles.jsx
+++ client/src/components/allProfiles/AllProfiles.jsx
@@ -1,21 +1,43 @@
 import {useContext, useEffect, useState} from "react";
 import * as profilService from '../../services/profilService.js';
 import ItemsProfile from "./ItemsProfile.jsx";
+import Pagination from "../myCerificates/Pagination.jsx";
 
 
 export default function AllProfiles() {
+
 	const [profiles, setProfiles] = useState([]);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [itemsPerPage, setItemsPerPage] = useState(8);
 
 	useEffect(() => {
-		profilService.getAll().then(result => setProfiles(result));
+		profilService.getAll()
+			.then(result => result = setProfiles(result))
 	}, []);
+
+
+	const indexOfLastProfile = currentPage * itemsPerPage;
+	const indexOfFirstProfil = indexOfLastProfile - itemsPerPage;
+	const currentProfiles = profiles.slice(indexOfFirstProfil, indexOfLastProfile);
+
+	const paginate = pageNumber => setCurrentPage(pageNumber)
 
 	return (
 		<section id='catalogPage'>
 			<h1>Users</h1>
 
-			{profiles.length === 0 ? <p>No found Users!</p>: profiles.map(profil => <ItemsProfile key={profil._id} {...profil}/>)}
+			{profiles.length === 0 ?
+				<p>No found Users!</p>
+				:
+				profiles.map(profil => <ItemsProfile key={profil._id} {...profil}/>)}
+			<Pagination
+				itemsPerPage={itemsPerPage}
+				totalItems = {profiles.length}
+				paginate={paginate}
+				currentPage={currentPage}
+			/>
 		</section>
 	)
+
 
 }
