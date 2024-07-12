f556b42a6e6a730452e8d590538eb8d88ba5340e
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-07-12 11:35:19 +0200

    Displaying a loading indicator before profiles are loaded

--- client/src/components/allProfiles/AllProfiles.jsx
+++ client/src/components/allProfiles/AllProfiles.jsx
@@ -9,35 +9,41 @@ export default function AllProfiles() {
 	const [profiles, setProfiles] = useState([]);
 	const [currentPage, setCurrentPage] = useState(1);
 	const [itemsPerPage, setItemsPerPage] = useState(8);
+	const [isLoading, setIsLoading] = useState(true)
 
 	useEffect(() => {
 		profilService.getAll()
-			.then(result => result = setProfiles(result))
-	}, []);
+			.then(result => {
+				setProfiles(result);
+				setIsLoading(false);
+			})
+
+			}, []);
 
 
-	const indexOfLastProfile = currentPage * itemsPerPage;
-	const indexOfFirstProfil = indexOfLastProfile - itemsPerPage;
-	const currentProfiles = profiles.slice(indexOfFirstProfil, indexOfLastProfile);
+		const indexOfLastProfile = currentPage * itemsPerPage;
+		const indexOfFirstProfil = indexOfLastProfile - itemsPerPage;
+		const currentProfiles = profiles.slice(indexOfFirstProfil, indexOfLastProfile);
 
-	const paginate = pageNumber => setCurrentPage(pageNumber)
+		const paginate = pageNumber => setCurrentPage(pageNumber)
 
-	return (
-		<section id='catalogPage'>
-			<h1>Users</h1>
+		return (
+			<section id='catalogPage'>
+				<h1>Users</h1>
+				{isLoading ? <p>Loading...</p> : profiles.length === 0 ?
+					<p>No found Users!</p>
+					:
+					profiles.map(profil => <ItemsProfile key={profil._id} {...profil}/>)
+				}
+				<Pagination
+					itemsPerPage={itemsPerPage}
+					totalItems={profiles.length}
+					paginate={paginate}
+					currentPage={currentPage}
+				/>
 
-			{profiles.length === 0 ?
-				<p>No found Users!</p>
-				:
-				profiles.map(profil => <ItemsProfile key={profil._id} {...profil}/>)}
-			<Pagination
-				itemsPerPage={itemsPerPage}
-				totalItems = {profiles.length}
-				paginate={paginate}
-				currentPage={currentPage}
-			/>
-		</section>
-	)
+			</section>
+		)
 
 
 }
