4775c02ea3d65dd2e74e85ebf656bf343ab42e00
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-07-13 19:00:02 +0200

    add a new Model Chat

---
+++ server/models/Chat.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const chatSchema = mongoose.Schema({
+	title: {
+		type: String,
+		require: true,
+		validator: {
+			validate: (value) => {
+				return value.length > 2 && value.length < 20;
+			},
+			message: 'Title must be between 2 and 20 characters'
+		}
+	},
+	sender: {
+		type: Object.Types.ObjectId,
+		ref: "Profil",
+	},
+	receiver: {
+		type: Object.Types.ObjectId,
+		ref: "Profil",
+	},
+	messages: [{
+		type: Object.Types.ObjectId,
+		ref: "Message",
+	}]
+}, {timestamps: true});
+
+const Chat = mongoose.model('Chat', chatSchema);
+
+module.exports = Chat;
