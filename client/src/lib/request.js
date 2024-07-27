98cfbcd7e368110b2f099e8b1a60ef50874da3bb
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-07-27 09:21:29 +0200

    update info by request

--- client/src/lib/request.js
+++ client/src/lib/request.js
@@ -1,13 +1,15 @@
+// Function to build request options including headers and body
 const buildOptions = (data) => {
 	const options = {};
 
+	// If data is provided, set the body and content-type header
 	if (data) {
 		options.body = JSON.stringify(data);
 		options.headers = {'content-type': 'application/json'};
 	}
 
+	// Get the token from localStorage and add it to the headers
 	const token = localStorage.getItem('accessToken');
-
 	if (token) {
 		options.headers = {
 			...options.headers,
@@ -15,24 +17,31 @@ const buildOptions = (data) => {
 		};
 	}
 
-	return options
+	return options;
 }
 
+// Function to make an HTTP request
 export default async function request(method, url, data) {
-	const response = await  fetch(url, {
+	// Make the fetch request with the provided method, url, and options
+	const response = await fetch(url, {
 		method,
 		credentials: 'include',
 		...buildOptions(data)
 	});
 
+	// If the response status is 204 (No Content), return an empty object
 	if (response.status === 204) {
 		return {};
 	}
+
+	// Parse the JSON response
 	const result = await response.json();
 
+	// If the response is not ok, throw an error with the result
 	if (!response.ok) {
 		throw result;
 	}
 
+	// Return the result
 	return result;
 }
