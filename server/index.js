cb6e7c0a79678231da872997f6c82154c1592340
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-08-04 17:46:21 +0200

    post to mongoDB Atlas

--- server/index.js
+++ server/index.js
@@ -2,12 +2,11 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const User = require('./models/User');
 const cors = require('cors');
 const router = require('./router');
 
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors({
 	origin: 'http://localhost:5173',
@@ -16,13 +15,14 @@ app.use(cors({
 
 app.use(express.json());
 
-
-mongoose.connect('mongodb://localhost:27017/certify-me');
+const dbURI = process.env.MONGODB_URI || 'mongodb+srv://blackangel9304:kC2Ob9WACFGd1dxb@cluster0.zsiluvc.mongodb.net/';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 mongoose.connection.on('connected', () => console.log('DB is connected'));
 mongoose.connection.on('disconnected', () => console.log('DB is disconnected'));
-mongoose.connection.on('error', (err) => console.log(err))
+mongoose.connection.on('error', (err) => console.log(err));
 
 app.use(router);
 
-app.listen(3030, () => console.log('Server started in port 3030'));
+const port = process.env.PORT || 3030;
+app.listen(port, () => console.log(`Server started on port ${port}`));
